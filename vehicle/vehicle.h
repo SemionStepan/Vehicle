036236fd0a166f8507ecabad9d758e694cf98dcf
SemionStepan <95352195+SemionStepan@users.noreply.github.com>
2023-08-11 16:07:08 +0300

    Add files via upload

--- vehicle/vehicle.h
+++ vehicle/vehicle.h
@@ -7,10 +7,10 @@ using namespace std;
 
 class Vehicle
 {
+public:
     string* _mark;
     int _numberOfCylinders;
     long _power;
-public:
     Vehicle();
     Vehicle( string* mark, int numberOfCylinders, long power);
     Vehicle( string* mark, int numberOfCylinders);
@@ -28,9 +28,31 @@ public:
 
 class Truck : public Vehicle
 {
-    int _bodyLoadCapacity;
+    short _bodyLoadCapacity;
 public:
     Truck();
+    Truck( string* mark, int numberOfCylinders, long power, short bodyLoadCapacity);
+    Truck( string* mark, int numberOfCylinders, long power);
+    Truck( string* mark, int numberOfCylinders, short bodyLoadCapacity);
+    Truck( string* mark, long power, short bodyLoadCapacity);
+    Truck( int numberOfCylinders, long power, short bodyLoadCapacity);
+    Truck( string* mark, int numberOfCylinders);
+    Truck( string* mark, long power);
+    Truck( int numberOfCylinders, long power);
+    Truck( string* mark, short bodyLoadCapacity);
+    Truck( int numberOfCylinders, short bodyLoadCapacity);
+    Truck( long power, short bodyLoadCapacity);
+    Truck(string* mark);
+    Truck(int numberOfCylinders);
+    Truck(long power);
+    Truck( short bodyLoadCapacity);
+
+    ~Truck();
+
+    void out();
+
+    string* setMark();
+    short setBodyLoadCapacity();
 
 };
 
